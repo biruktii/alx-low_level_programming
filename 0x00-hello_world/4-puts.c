06ec6d72a154b74cf11acafa39de1f1dfa5963e8
biruktii <biruktawitchernet@gmail.com>
2022-10-13 15:04:26 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-       char test_str[] = "\"Programming is like building a multilingual puzzle";
+\tchar test_str[] = "\"Programming is like building a multilingual puzzle";
 
-       puts(test_str);
-       return (0);
+\tputs(test_str);
+      \treturn (0);
 }
