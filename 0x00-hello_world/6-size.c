906631f6165f98cd19ffc482245965d7065dc5c4
biruktii <biruktawitchernet@gmail.com>
2022-10-13 16:04:05 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,18 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{  
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0); 
+{
+  
+char c;
+int d;
+long int ld;  
+long long int lld; 
+float f;
+ 
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));  
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));  
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(ld));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lld));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));  
+return (0);  
 }
