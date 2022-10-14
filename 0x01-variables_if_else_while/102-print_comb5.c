e0cb57dd19a2fad91d50d46e3444a093b3a2b4fa
biruktii <biruktawitchernet@gmail.com>
2022-10-14 13:41:57 -0700

    software

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ for (a = 48; a <= 57; a++)
 {
 for (b = 48; b <= 57; b++)
 {
-for (c = 48; c <= 57; c++)	    
+for (c = 48; c <= 57; c++)
 {
 for (d = 48; d <= 57; d++)
 {
@@ -21,7 +21,7 @@ if (((c + d) > (a + b) &&  c >= a) || a < c)
 putchar(a);
 putchar(b);
 putchar(' ');
-putchar(c);		      
+putchar(c);
 putchar(d);
 if (a + b + c + d == 227 && a == 57)
 {
