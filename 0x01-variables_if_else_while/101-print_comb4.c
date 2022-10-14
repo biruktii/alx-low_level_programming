fcac7ad58129c3ee7cc20d735d82a71cbc9554d3
biruktii <biruktawitchernet@gmail.com>
2022-10-14 13:13:23 -0700

    success

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Prints 3 combination of numbers
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+
+int i, j, k;
+for (i = '0'; i <= '9'; i++)
+{
+for (j = '0'; j <= '9'; j++)
+{
+for (k = '0'; k <= '9'; k++)
+{
+if (c < i && i < k)        	
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (i != '7')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
