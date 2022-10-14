8d7c5383520f96698d31f47d56c162e5c47922f4
biruktii <biruktawitchernet@gmail.com>
2022-10-14 12:40:03 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 
-  char d;
-for (d  =    '0'; d     <= '9'; d    ++)
-putchar(d    );
-for (d     = 'a'; d     <= 'f'; d    ++)
-putchar(d    );
+char d;
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+for (d = 'a'; d <= 'f'; d++)
+putchar(d);
 putchar('\n');
 return (0);
 }
