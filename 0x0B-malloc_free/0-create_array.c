28f0bb20b48bcd35064c7b17ac030f257ce8b4ea
biruktii <biruktawitchernet@gmail.com>
2022-11-08 19:47:01 +0000

    create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,8 +4,8 @@
  *  create_array - creates an array of chars
  *  @size: The size of the array
  *  @c: The char to fill in the array
- * 
- *  Return: The array filled            
+ *
+ *  Return: The array filled
  */
 char *create_array(unsigned int size, char c)
 {
@@ -20,6 +20,6 @@ return (NULL);
 for (i = 0; i < size; i++)
 {
 s[i] = c;
-}								
+}					
 return (s);
 }
