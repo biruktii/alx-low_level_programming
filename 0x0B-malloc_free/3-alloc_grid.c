665bd8b65d237cec25f2886e8bf34755aaa670f9
biruktii <biruktawitchernet@gmail.com>
2022-11-08 20:28:40 +0000

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,7 +9,7 @@
  */
 int **alloc_grid(int width, int height)
 {
-	
+
 int i, j, k, l;
 int **a;
 if (width <= 0 || height <= 0)
@@ -28,7 +28,7 @@ if (a[i] == NULL)
 for (j = i; j >= 0; j--)
 {
 free(a[j]);
-}		
+}
 free(a);
 return (NULL);
 }
@@ -37,7 +37,7 @@ for (k = 0; k < height; k++)
 {
 for (l = 0; l < width; l++)
 {
-a[k][l] = 0;					
+a[k][l] = 0;
 }
 }
 return (a);
