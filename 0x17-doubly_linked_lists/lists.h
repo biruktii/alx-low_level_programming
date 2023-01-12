c71836f8c580e0a736e0f683c0a709d9baf3bc97
biruktii <biruktawitchernet@gmail.com>
2023-01-12 19:59:07 +0000

    add node

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,11 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/*
- *   File: lists.h
- *    Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -33,4 +28,6 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-int delete_dnodeint_at_in
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+#endif /* LISTS_H */
